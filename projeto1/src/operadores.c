b960f8d317c0ad94cd734e4966fbe31d4095e2f9
FabioOSJunior <jr.fabio.oliveira.silva@gmail.com>
2026-03-10 08:16:46 -0300

    aprendizado de operadores em C

--- projeto1/src/operadores.c
+++ projeto1/src/operadores.c
@@ -1,2 +1,26 @@
 // Uso de operadores matemáticos em c // 
 #include <stdio.h>
+
+int main () {
+    int numero1, numero2;
+    int soma, subtracao, multiplicacao, divisao;
+
+    printf("Entre com o numero 1: ");
+    scanf("%d", &numero1);
+    printf("Entre com o numero 2: ");
+    scanf("%d", &numero2);
+
+    soma = numero1 + numero2; // operação soma
+
+    subtracao = numero1 - numero2; // operação subtração
+
+    multiplicacao = numero1 * numero2; // operação multiplicação
+
+    divisao = numero1 / numero2; // operação divisão
+
+    printf("A soma é: %d\n", soma);
+    printf("A subtração é: %d\n", subtracao);
+    printf("A multiplicação é: %d\n", multiplicacao);
+    printf("A divisão é: %d\n", divisao);
+
+}
